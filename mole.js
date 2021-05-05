bafe97f33f71d01168813ef75ea59a50d6f48037
Nathan Darrow <nathanadarrow@gmail.com>
2021-05-05 12:33:36 -0700

    Finishing random pop up

--- mole.js
+++ mole.js
@@ -1,15 +1,29 @@
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
 
-//     setInterval(() => {
-//         const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//         for (let moleHead of moleHeads) {
-//             moleHead.classList.toggle('wgs__mole-head--hidden');
-//         }
-//     }, 1000)
+  //     setInterval(() => {
+  //         const moleHeads = document.querySelectorAll('.wgs__mole-head');
+  //         for (let moleHead of moleHeads) {
+  //             moleHead.classList.toggle('wgs__mole-head--hidden');
+  //         }
+  //     }, 1000)
 
-// });
+  function popUpRandomMole() {
+    const moleHeads = document.querySelectorAll(".wgs__mole-head");
+    //formula: Math.random() * (max - min + 1) + min
+    //min = 0 max = 7;
+    let random = Math.floor(Math.random()* 8);
+    let currHead = moleHeads[random];
+    currHead.classList.remove('wgs__mole-head--hidden');
+    setTimeout(() => {
+        hideMole(currHead);
+    }, 1000)
+  }
 
-function popUpRandomMole() {
-    const moleHEads = document.querySelectorAll(".wgs__mole-head");
-    let random = Math.floor(Math.random(0, 7));
-}
+  function hideMole(currHead) {
+      currHead.classList.add('wgs__mole-head--hidden');
+      setTimeout(() => {
+        popUpRandomMole();
+    }, 1000)
+  }
+
+});
